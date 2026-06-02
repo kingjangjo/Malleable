5ce4e3d1d529890f70c9d0e1d68acb3bee647dd8
kingjangjo <araloveasalll@gmail.com>
2026-06-03 01:58:27 +0900

    AddPipeAsset

---
+++ Assets/01_Scripts/05_Chamber/ChamberNumbers/Chamber_01.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Chamber_01 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
